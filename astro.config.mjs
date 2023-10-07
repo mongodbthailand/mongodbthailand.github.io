dffcab339aa814a67b77ca0c98b24e9b1a03742b
Jatesadakarn Saengrat <champjss@gmail.com>
2023-10-07 17:07:22 +0700

    Correct deployment (#2)
    Correct deployment by removing base configuration
    
    https://docs.astro.build/en/guides/deploy/github/

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mongodbthailand.com',
-  base: '/',
   integrations: [
     starlight({
       title: 'MongoDB Thailand',
