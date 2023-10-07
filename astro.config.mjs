dfdf833b3ad75f59dfbea66d2ec1481a63e0b071
Jatesadakarn Saengrat <champjss@gmail.com>
2023-10-07 16:50:34 +0700

    Add basic guides to write

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,10 +23,7 @@ export default defineConfig({
       sidebar: [
         {
           label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
+          autogenerate: { directory: 'guides' },
         },
         {
           label: 'Hacktoberfest 2023',
